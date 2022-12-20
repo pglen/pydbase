921f2773cf988b0493d72eb570bd910597947084
Peter <peterglen99@gmail.com>
2022-12-19 21:09:55 -0500

    autocheck

--- test_create.py
+++ test_create.py
@@ -4,27 +4,29 @@ import pytest, twincore, pypacker
 core = None
 
 def setup_function(function):
+    #assert tmp_path == ""
+    #assert core != 0
+    pass
+
+def test_create():
     global core
-    print("setting up", function)
     core = twincore.TwinCore()
     assert core != 0
 
-def test_one():
-    print("one")
-    assert 1
-
 def test_write():
     print("write", core)
-    core.save_data("111", "222")
+    ret = core.save_data("111", "222")
+    assert ret != 0
+
+def test_read():
+    print("read", core)
+    ret = core.get_rec(2)
+    assert ret != 0
 
 def test_create_file(tmp_path):
     #assert tmp_path == ""
     print("tmp_path", tmp_path)
     #assert core == 0
     pass
-
-#print(dir(pytest))
-#print(tmp_path)
-
 
 # EOF
