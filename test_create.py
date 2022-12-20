20561f80befc50960cf2bb36f25a63449224a86a
Peter <peterglen99@gmail.com>
2022-12-19 21:31:39 -0500

    Started pytests

--- test_create.py
+++ test_create.py
@@ -1,5 +1,5 @@
 
-import pytest, twincore, pypacker
+import pytest, twincore, pypacker, os
 
 core = None
 
@@ -10,18 +10,30 @@ def setup_function(function):
 
 def test_create():
     global core
-    core = twincore.TwinCore()
+    os.remove("data/tests.pydb")
+    os.remove("data/tests.pidx")
+
+    core = twincore.TwinCore("data/tests.pydb")
     assert core != 0
 
 def test_write():
     print("write", core)
+    ret = core.save_data("1111", "2222")
+    assert ret != 0
+    ret = core.save_data("11111", "22222")
+    assert ret != 0
     ret = core.save_data("111", "222")
+    assert ret != 0
+
+def test_get():
+    print("get", core)
+    ret = core.get_rec(2)
     assert ret != 0
 
 def test_read():
     print("read", core)
-    ret = core.get_rec(2)
-    assert ret != 0
+    ret = core.retrieve("111")
+    assert ret == [[b'111', b'222']]
 
 def test_create_file(tmp_path):
     #assert tmp_path == ""
