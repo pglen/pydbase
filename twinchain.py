05c01140b232a690ac95a01ab2db064aceaf0ea2
Peter Glen <peterglen@gmail.com>
2023-03-26 22:54:40 -0500

    autocheck

--- twinchain.py
+++ twinchain.py
@@ -46,7 +46,7 @@ class TwinChain(TwinCore):
         arrx.append(keyx)
         arrx.append(strx)
 
-    def _pad(self, strx, lenx=10):
+    def _pad(self, strx, lenx=8):
         ttt = len(strx)
         if ttt >= lenx:
             return strx
@@ -56,9 +56,22 @@ class TwinChain(TwinCore):
     def append(self, datax):
 
         #print("Appending data", data)
-
         if type(datax) == str:
             datax = datax.encode(errors='strict')
+
+        self.old_fff = ""
+        # Get last data from db
+        sss = self.getdbsize()
+        #print("sss", sss)
+        if sss:
+            ooo = self.get_rec(sss-1)
+            #print("ooo", ooo)
+            decoded = self.packer.decode_data(ooo[1])
+            #self.dump_rec(decoded[0])
+            self.old_fff = self.get_field(decoded[0], "hash256")
+            self.old_time = self.get_field(decoded[0], "now")
+            print("old_fff", self.old_fff)
+            print("old_time", self.old_time)
 
         # Produce data structure
         aaa = []
@@ -72,19 +85,37 @@ class TwinChain(TwinCore):
 
         hh = hashlib.new("sha256")
         hh.update(datax)
-        self._key_n_data(aaa, "hash256", hh.hexdigest())
+        self.new_fff = hh.hexdigest()
+        self._key_n_data(aaa, "hash256", self.new_fff)
+
+        dd = hashlib.new("md5")
+        dd.update(datax)
+        self._key_n_data(aaa, "md5", dd.hexdigest())
+
+        #print("old_fff", self.old_fff, "new_fff", self.new_fff)
+        backlink = (self.old_fff + self.new_fff + fdt).encode()
+        hh.update(backlink)
+        self.new_fff = hh.hexdigest()
+        self._key_n_data(aaa, "backlink", hh.hexdigest())
 
         encoded = self.packer.encode_data("", aaa)
-        print(encoded)
+        #print(encoded)
 
         self.save_data(header, encoded)
 
         bbb = self.packer.decode_data(encoded)
         #print(bbb[0])
+        self.dump_rec(bbb[0])
 
-        for aa in range(len(bbb[0])//2):
-            print(self._pad(bbb[0][2*aa]), "=", bbb[0][2*aa+1])
+    def dump_rec(self, bbb):
+        for aa in range(len(bbb)//2):
+            print(self._pad(bbb[2*aa]), "=", bbb[2*aa+1])
 
+    def get_field(self, bbb, field):
+        for aa in range(len(bbb)//2):
+            #print("bbb", bbb[2*aa], bbb[2*aa+1])
+            if bbb[2*aa] == field:
+                return bbb[2*aa+1]
 
     def __del__(self):
         ''' Override for now '''
